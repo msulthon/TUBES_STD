ca2f088e73d216b855d72984796532e0c45614fe
msulthon <muhammadsulthon82@gmail.com>
2020-04-28 12:26:33 +0700

    Revert "relasi"
    This reverts commit 77e16e8191f4cea123db0f46f1255a923d9a0c64.

--- TUBES_STD/listRelasi.h
+++ TUBES_STD/listRelasi.h
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "listParrent.h"
+//include parrent here//
 #include "listChild.h"
 
 using namespace std;
@@ -7,27 +7,13 @@ using namespace std;
 typedef struct Relasi* relateAddress;
 
 struct relasi {
-	address_parent warteg;
+	//address parrent disini//
 	addressChild mamin;
 	relateAddress next;
 	relateAddress prev;
 };
 
-
 struct listRelasi {
 	relateAddress first;
 	relateAddress last;
 };
-
-void createListRelasi(ListRelasi &L);
-relateAddress CeateElmrelasi(address_parent P, addressChild Q);
-void insertRelasi(ListRelasi &L, relateAddress &R);
-relateAddress findElmRelasiParent(ListRelasi &L, string out);
-relateAddress findElmRelasiChild(ListRelasi &L, string out);
-void printInfoRelasi (ListRelasi L);
-
-void printRelasiParent(ListRelasi L, string out);
-void printRelasiChild(ListRelasi L, string out);
-
-void HapusRelasiParentSemua(ListRelasi &L, address_parent P);
-void HapusRelasiChildSemua(ListRelasi &L, addressChild Q);
