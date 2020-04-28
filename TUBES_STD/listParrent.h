d030628739cd48ac8574f51cc4c845bebe3558d1
msulthon <muhammadsulthon82@gmail.com>
2020-04-28 12:26:42 +0700

    Revert "p"
    This reverts commit 02d37393d6450b3d7f8acd76740bd333bc44330c.

--- TUBES_STD/listParrent.h
+++ TUBES_STD/listParrent.h
@@ -22,9 +22,6 @@ struct List_Parent{
     address_parent last;
 };
 
-void inputWarteg(List_Parent L, infotypeParent &in);
-string generatorIDParent();
-
 void createListParent(List_Parent &L);
 address_parent createElmParent(infotypeParent in);
 
@@ -41,4 +38,3 @@ void deleteLastParent(List_Parent &L, address_parent &P);
 address_parent findElmParent(List_Parent &L, string X);
 void printInfoParent(List_Parent L);
 void deleteSpesifikParent(List_Parent &L, string X);
-void deallocateParent(address_parent &P);
